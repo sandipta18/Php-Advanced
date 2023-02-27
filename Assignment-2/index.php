a3b761dcdc296ec5d1a180de28d448db1905def5
sandipta18 <sandipta.sardar@innoraft.com>
2023-02-27 18:30:25 +0530

    FT2023-58 : Implemented Assignment 2 .

---
+++ Assignment-2/index.php
@@ -0,0 +1,44 @@
+<?php 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mailer Tutorial</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <input type="email" name="mail" placeholder="Enter Email">
+        <input type="submit" name="submit">
+    </form>
+    
+</body>
+<?php 
+require('vendor/autoload.php'); 
+
+    if(isset($_POST['submit'])){
+      $mail = new PHPMailer(true);
+      $mail->isSMTP();
+      $mail->Host = 'smtp.gmail.com';
+      $mail->SMTPAuth = true;
+      $mail->Username = 'sandiptasardar99@gmail.com';
+      $mail->Password = 'tbwczrfesfmfyxge';
+      $mail->SMTPSecure = 'ssl';
+      $mail->Port = 465;
+      $mail->setFrom('sandiptasardar99@gmail.com');
+      $mail->addAddress($_POST['mail']);
+      $mail->isHTML(true);
+      $mail->Subject = 'Thank you for Subscribing';
+      $mail->Body = 'Welcome User';
+      $mail->send();
+      echo "success";
+    }
+
+?>
+</html>
